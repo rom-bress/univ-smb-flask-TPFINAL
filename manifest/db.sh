97ed617c6719e46aee5284330b0ae177acc61a97
Romain Bressy <romain.bress@gmail.com>
2023-03-24 09:02:46 +0000

    TPFINAL

--- manifest/db.sh
+++ manifest/db.sh
@@ -10,5 +10,29 @@ sudo mysql -e "CREATE DATABASE IF NOT EXISTS config_generator"
 sudo mysql -e "GRANT ALL ON *.* TO 'identity'@'localhost' IDENTIFIED BY 'passwordDbUniv01' WITH GRANT OPTION;"
 # Create application account
 sudo mysql -e "GRANT ALL ON *.* TO 'config_generator'@'localhost' IDENTIFIED BY 'passwordDbUniv01' WITH GRANT OPTION;"
+
+#Création des tables dans identity
+sudo mysql -e "CREATE TABLE authentification (
+                        id_a          INTEGRER       PRIMARY KEY,
+                        username      VARCHAR(20)    not null,
+                        password      VARCHAR(50)    not null
+);"
+
+sudo mysql -e "CREATE TABLE profil (
+                        id_p              INTEGRER       PRIMARY KEY,
+                        Prénom            VARCHAR(20)    not null,
+                        Nom               VARCHAR(20)    not null,
+                        Date_Naissance    DATE           not null,
+                        id_a              INTEGRER       not null,
+                        FOREIGN KEY (id_a) REFERENCES CUSTOMERS (id_a)
+);"
+
+sudo mysql -e "CREATE TABLE loadbalancer (
+                        id_l              INTEGRER       PRIMARY KEY,
+                        server            VARCHAR(100)   not null,
+                        FOREIGN KEY (id_a) REFERENCES CUSTOMERS (id_a)
+);"
+
+
 # Make our changes take effect
 sudo mysql -e "FLUSH PRIVILEGES"
--- src/flask-api/api.py
+++ src/flask-api/api.py
@@ -1,7 +1,42 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from yourapp import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///identity.db'
+db = SQLAlchemy(app)
 
-@app.route("/")
-def hello():
-    return "Hello, API!"
+class Authentification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+class Profil(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(80), nullable=False)
+    prenom = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    authentification_id = db.Column(db.Integer, db.ForeignKey('authentification.id'), nullable=False)
+    authentification = db.relationship('Authentification', backref=db.backref('profil', lazy=True))
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///second_db.db'
+db = SQLAlchemy(app)
+
+class WebServer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    ip_address = db.Column(db.String(120), nullable=False)
+
+class LoadBalancer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    ip_address = db.Column(db.String(120), nullable=False)
+
+class ReverseProxy(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    ip_address = db.Column(db.String(120), nullable=False)
+
+
+db.create_all()
