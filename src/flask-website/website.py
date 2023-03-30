a15d7a0dff9c6f733a0f6f38671dd8fa915bc5c5
Romain Bressy <romain.bress@gmail.com>
2023-03-30 11:28:55 +0000

    cool

--- src/flask-website/website.py
+++ src/flask-website/website.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def start(): 
     return render_template('start.html')
+
+@app.route("/authentification")
+def authentification(): 
+    return render_template('authentification.html')
     
